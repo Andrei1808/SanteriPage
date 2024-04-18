91bd65459eb81e27c1a67d8fd9b15147373d9c93
Andrei <kaliarovich93@.gmail.com>
2024-04-18 22:42:53 +0300

    feat: add Routers

---
+++ SanteriWebPage/src/routers/Routers.tsx
@@ -0,0 +1,18 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "../pages/Home/Home";
+
+
+export default function Routers() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="home" element={<Home />} />
+      {/* <Route path="cart" element={<Cart />} />
+      <Route path="wishlist" element={<WishList />} />
+      <Route path="login" element={<Login />} />
+      <Route path="productDetails" element={<ProductDetails />} />
+      <Route path="shop" element={<Shop />} />
+      <Route path="signup" element={<Signup />} /> */}
+    </Routes>
+  );
+}
