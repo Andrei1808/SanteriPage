12acd414b0a154889b637b3d75e43f3579df3075
Andrei <kaliarovich93@.gmail.com>
2024-04-18 21:52:13 +0300

    fix: eslint config

--- SanteriWebPage/.eslintrc.cjs
+++ SanteriWebPage/.eslintrc.cjs
@@ -1,18 +1,64 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "airbnb",
+    "airbnb/hooks",
+    "airbnb-typescript",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  "prettier/prettier": [
+    "error",
+    {
+      endOfLine: "auto",
+    },
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs,ts}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+  },
+  plugins: ["@typescript-eslint", "react", "prettier"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "@typescript-eslint/no-explicit-any": "error",
+    "no-underscore-dangle": "off",
+    "no-extraneous-dependencies": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "error",
+    "import/no-cycle": "off",
+    "react/jsx-props-no-spreading": "off",
+    "@typescript-eslint/typedef": [
+      "error",
+      {
+        variableDeclaration: true,
+        arrowParameter: false,
+        propertyDeclaration: true,
+        variableDeclarationIgnoreFunction: true, // Если хотите игнорировать функции
+        parameter: false, // Если хотите требовать типы у параметров функций
+      },
+    ],
+    "no-param-reassign": [
+      "error",
+      { props: true, ignorePropertyModificationsFor: ["state"] },
     ],
   },
-}
+};
