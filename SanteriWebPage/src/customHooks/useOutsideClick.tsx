5293975d02c1531577827e898e1f4a037e26931f
Andrei <kaliarovich93@.gmail.com>
2024-04-22 23:11:15 +0300

    feat: add custom hook useOutsideClick

---
+++ SanteriWebPage/src/customHooks/useOutsideClick.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef, useState } from "react";
+
+export default function outsideClick(initialIsVisible: boolean) {
+  const [isVisible, setIsVisible] = useState(initialIsVisible);
+  const ref = useRef<HTMLUListElement>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    console.log(ref.current);
+    if (ref.current && !ref.current.contains(event.target as Node)) {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click",  handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", () => handleClickOutside, true);
+    };
+  });
+
+  return { ref, isVisible, setIsVisible };
+}
+
+// Может ьудет не нужен, тогда удали!!!
