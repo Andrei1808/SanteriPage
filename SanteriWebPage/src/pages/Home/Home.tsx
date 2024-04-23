05761718eb80dedd8257dcbe732787064dba8074
Andrei <kaliarovich93@.gmail.com>
2024-04-23 23:49:19 +0300

    refactor: refactor code

--- SanteriWebPage/src/pages/Home/Home.tsx
+++ SanteriWebPage/src/pages/Home/Home.tsx
@@ -1,6 +1,27 @@
 import React from "react";
 import s from "./Home.module.scss";
+import { NavLink } from "react-router-dom";
+import Button from "../../components/Button/Button";
+import Services from "../../components/Services/Services";
+
 
 export default function Home() {
-  return <div className={s.s}>Home</div>;
+  return (
+    <section className={s.home}>
+      <div className={s.wrapper}>
+        <div className={s.info}>
+          <h2 className={s.title}>
+            Onko jotain mennyt rikki?
+            <br /> Vai tarvitsetko apua?
+            <br /> <span className={s.mainText}></span>
+          </h2>
+          <p className={s.phoneNumber}>+358 (50) 554-52-05</p>
+          <Button type="button" className={s.phoneNumberBtn}>
+            <NavLink to="tel:+358 (50) 554-52-05">Soita meille nyt!</NavLink>
+          </Button>
+        </div>
+      </div>
+      <Services />
+    </section>
+  );
 }
