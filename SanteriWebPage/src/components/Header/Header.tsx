410a4328294bfa3d5eb609492e1059355bbbe7c7
Andrei <kaliarovich93@.gmail.com>
2024-04-23 23:48:27 +0300

    feat: add hover effect

--- SanteriWebPage/src/components/Header/Header.tsx
+++ SanteriWebPage/src/components/Header/Header.tsx
@@ -11,46 +11,54 @@ export default function Header() {
     <header className={s.header}>
       <div className={s.wrapper}>
         <div className={s.logo}>
-          <img src={logo} alt="logo-header" />
+          <NavLink to="home">
+            <img src={logo} alt="logo-header" />
+          </NavLink>
         </div>
 
         <div className={s.navigation}>
           <ul className={s.menu}>
             <li className={s.navItem}>
-              <NavLink to="home">etusivu</NavLink>
+              <NavLink className={s.menuLink} to="home">
+                etusivu
+              </NavLink>
             </li>
             <li
               className={s.navItem}
               onMouseOver={() => setIsVisible(true)}
               onMouseLeave={() => setIsVisible(false)}
             >
-              <NavLink to="services">palvelut</NavLink>
+              <NavLink className={s.menuLink} to="services">
+                palvelut
+              </NavLink>
 
               {isVisible && (
                 <ul className={s.servicesItems}>
-                  <li className="servicesItem">
+                  <li className={s.servicesItem}>
                     <NavLink to="computer-service">tietokone</NavLink>
                   </li>
-                  <li className="servicesItem">
+                  <li className={s.servicesItem}>
                     <NavLink to="renovation-service">remontti</NavLink>
                   </li>
-                  <li className="servicesItem">
+                  <li className={s.servicesItem}>
                     <NavLink to="cleaning-service">siivous</NavLink>
                   </li>
-                  <li className="servicesItem">
+                  <li className={s.servicesItem}>
                     <NavLink to="yard-service">pihä</NavLink>
                   </li>
-                  <li className="servicesItem">
+                  <li className={s.servicesItem}>
                     <NavLink to="cctv-service">videovalvonta</NavLink>
                   </li>
-                  <li className="servicesItem">
+                  <li className={s.servicesItem}>
                     <NavLink to="photos-service">valokuvaaja</NavLink>
                   </li>
                 </ul>
               )}
             </li>
             <li className={s.navItem}>
-              <NavLink to="contacts">yhteystiedot</NavLink>
+              <NavLink className={s.menuLink} to="contacts">
+                yhteystiedot
+              </NavLink>
             </li>
           </ul>
         </div>
