c7f151ad31c576b738d20ca1861df1e542cb3e1a
Andrei <kaliarovich93@.gmail.com>
2024-04-23 23:47:28 +0300

    feat: add store for services

---
+++ SanteriWebPage/src/store/servicesData.ts
@@ -0,0 +1,44 @@
+export const services = [
+  {
+    title: "Tietokone palvelu",
+    description:
+      " Tarjoamme laadukkaita teknisiä palveluita, kuten uuden tietokoneen valmistelua, käyttöjärjestelmien asennusta ja vuosittaista huoltoa. Luota ammattitaitoiseen tiimiimme ja nauti luotettavasta ja korkeatasoisesta palvelusta!",
+    icon: "src/assets/icons/pc1.png",
+    id: "1",
+  },
+  {
+    title: "Remontti palvelu",
+    description:
+      " Kokoelma palveluitamme sisältää kaiken mitä tarvitset tilasi päivittämiseen: huonekalujen korjaamisesta ja verhoilusta aina asunnon remonttiin.",
+    icon: "src/assets/icons/renovation.png",
+    id: "2",
+  },
+  {
+    title: "Siivous palvelu",
+    description:
+      " Tiimimme tarjoaa laajan valikoiman siivouspalveluita erilaisiin tiloihin. Koosta ja haastavuudesta riippumatta olemme valmiita takaamaan tilasi puhtauden ja raikkauden. Luota meihin huolehtimaan kodistasi, toimistostasi tai mistä tahansa muusta paikasta, ja saat korkealaatuista palvelua ja moitteetonta tulosta.",
+    icon: "src/assets/icons/cleaning.png",
+    id: "3",
+  },
+  {
+    title: "Pihä palvelu",
+    description:
+      " Anna pihan aina kukoistaa puhtaudesta ja viihtyisyydestä huolehtivien ammattilaistemme avulla. Säästä ja säästä riippumatta olemme valmiita muuttamaan pihasi houkuttelevaksi ja hoidetuksi paikaksi. Luota meille pihan hoitoon, ja saat täydellisen järjestyksen ja moitteettoman ulkonäön.",
+    icon: "src/assets/icons/outside.png",
+    id: "4",
+  },
+  {
+    title: "Videovalvonta palvelu",
+    description:
+      "Luo luotettavuutta ja turvallisuutta tilaasi ammattitaitoisilla valvontajärjestelmien asennus- ja huoltopalveluillamme. Tarjoamme kattavia ratkaisuja kotisi, yrityksesi tai toimistosi suojaamiseksi, varmistaen korkean laadun ja luotettavuuden jokaisessa näkökulmassa.",
+    icon: "src/assets/icons/cctv.png",
+    id: "5",
+  },
+  {
+    title: "Valokuvaaja palvelu",
+    description:
+      " Sukella kauneuden ja tunteiden maailmaan valokuvapalveluidemme avulla. Taltioimme elämäsi kirkkaimmat hetket luomalla upeita ja ainutlaatuisia kuvia. Olipa kyse sitten ammattimaisista valokuvista yrityksellesi tai ikimuistoisista muotokuvista perhealbumiin, olemme valmiita toteuttamaan ideoitasi lämmöllä ja ammattitaidolla.",
+    icon: "src/assets/icons/photo.png",
+    id: "6",
+  },
+];
