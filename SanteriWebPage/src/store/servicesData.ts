c2b4c7daccae65c62547e5f9a55923e189ce6cb8
Andrei <kaliarovich93@.gmail.com>
2024-04-30 23:50:48 +0300

    feat: add images

--- SanteriWebPage/src/store/servicesData.ts
+++ SanteriWebPage/src/store/servicesData.ts
@@ -4,6 +4,7 @@ export const services = [
     description:
       " Tarjoamme laadukkaita teknisiä palveluita, kuten uuden tietokoneen valmistelua, käyttöjärjestelmien asennusta ja vuosittaista huoltoa. Luota ammattitaitoiseen tiimiimme ja nauti luotettavasta ja korkeatasoisesta palvelusta!",
     icon: "src/assets/icons/pc1.png",
+    img: "src/assets/images/services-page/pc-service.jpg",
     id: "1",
   },
   {
@@ -11,6 +12,7 @@ export const services = [
     description:
       " Kokoelma palveluitamme sisältää kaiken mitä tarvitset tilasi päivittämiseen: huonekalujen korjaamisesta ja verhoilusta aina asunnon remonttiin.",
     icon: "src/assets/icons/renovation.png",
+     img: "src/assets/images/services-page/renovation.jpg",
     id: "2",
   },
   {
@@ -18,6 +20,7 @@ export const services = [
     description:
       " Tiimimme tarjoaa laajan valikoiman siivouspalveluita erilaisiin tiloihin. Koosta ja haastavuudesta riippumatta olemme valmiita takaamaan tilasi puhtauden ja raikkauden. Luota meihin huolehtimaan kodistasi, toimistostasi tai mistä tahansa muusta paikasta, ja saat korkealaatuista palvelua ja moitteetonta tulosta.",
     icon: "src/assets/icons/cleaning.png",
+     img: "src/assets/images/services-page/cleaning.jpg",
     id: "3",
   },
   {
@@ -25,6 +28,7 @@ export const services = [
     description:
       " Anna pihan aina kukoistaa puhtaudesta ja viihtyisyydestä huolehtivien ammattilaistemme avulla. Säästä ja säästä riippumatta olemme valmiita muuttamaan pihasi houkuttelevaksi ja hoidetuksi paikaksi. Luota meille pihan hoitoon, ja saat täydellisen järjestyksen ja moitteettoman ulkonäön.",
     icon: "src/assets/icons/outside.png",
+     img: "src/assets/images/services-page/yard.jpg",
     id: "4",
   },
   {
@@ -32,6 +36,7 @@ export const services = [
     description:
       "Luo luotettavuutta ja turvallisuutta tilaasi ammattitaitoisilla valvontajärjestelmien asennus- ja huoltopalveluillamme. Tarjoamme kattavia ratkaisuja kotisi, yrityksesi tai toimistosi suojaamiseksi, varmistaen korkean laadun ja luotettavuuden jokaisessa näkökulmassa.",
     icon: "src/assets/icons/cctv.png",
+     img: "src/assets/images/services-page/cctv.jpg",
     id: "5",
   },
   {
@@ -39,6 +44,7 @@ export const services = [
     description:
       " Sukella kauneuden ja tunteiden maailmaan valokuvapalveluidemme avulla. Taltioimme elämäsi kirkkaimmat hetket luomalla upeita ja ainutlaatuisia kuvia. Olipa kyse sitten ammattimaisista valokuvista yrityksellesi tai ikimuistoisista muotokuvista perhealbumiin, olemme valmiita toteuttamaan ideoitasi lämmöllä ja ammattitaidolla.",
     icon: "src/assets/icons/photo.png",
+     img: "src/assets/images/services-page/photo.jpg",
     id: "6",
   },
 ];
