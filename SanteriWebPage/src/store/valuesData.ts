23c164bfa1d05bf2d4dedd7bbb487387f6b4b231
Andrei <kaliarovich93@.gmail.com>
2024-05-02 23:57:19 +0300

    feat: add values data

---
+++ SanteriWebPage/src/store/valuesData.ts
@@ -0,0 +1,27 @@
+export const valuesData = [
+  {
+    title: "Laatu",
+    description:
+      " Pyrimme korkeaan laatuun kaikessa, mitä teemme. Tavoitteenamme on tarjota asiakkaillemme ensiluokkaisia palveluita ja tuloksia.",
+  },
+  {
+    title: " Ammattitaito",
+    description:
+      "Tiimimme koostuu kokeneista asiantuntijoista, jotka ovat valmiita ratkaisemaan vaativimmatkin tehtävät. Suhtaudumme jokaiseen projektiin vakavasti ja ammattitaidolla. ",
+  },
+  {
+    title: "Luotettavuus ",
+    description:
+      "Pyrimme olemaan luotettava kumppani asiakkaillemme. Voit luottaa meihin missä tahansa tilanteessa, tietäen että liiketoimintasi on luotettavissa käsissä. ",
+  },
+  {
+    title: "Yksilöllinen lähestymistapa ",
+    description:
+      " Ymmärrämme, että jokainen asiakas on ainutlaatuinen, ja pyrimme tarjoamaan yksilöllisen lähestymistavan jokaiseen projektiin. Olemme valmiita kuuntelemaan mielipiteitänne ja mieltymyksiänne tyydyttääksemme tarpeenne.",
+  },
+  {
+    title: " Asiakastyytyväisyys",
+    description:
+      "On meille tärkeää, että jokainen asiakkaamme on täysin tyytyväinen työmme tulokseen. Teemme kaikkemme ylittääksemme asiakkaiden odotukset ja jättääksemme vain positiivisia kokemuksia. ",
+  },
+];
