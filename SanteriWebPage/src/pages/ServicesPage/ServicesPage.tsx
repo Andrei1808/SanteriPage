4c0f966dae9ea4e9e62f96f690a45b5cb09db465
Andrei <kaliarovich93@.gmail.com>
2024-05-03 23:51:34 +0300

    feat: add wish section on services page

--- SanteriWebPage/src/pages/ServicesPage/ServicesPage.tsx
+++ SanteriWebPage/src/pages/ServicesPage/ServicesPage.tsx
@@ -4,15 +4,21 @@ import ServicesPageCards from "./ServicesPageCards/ServicesPageCards";
 import Values from "./Values/Values";
 
 export default function ServicesPage() {
-    return (
-      <section className={s.servicesPage}>
-        <h2 className={s.title}>meidän palvelut</h2>
-        <h4 className={s.subtitle}>
-          Palvelumme kattavat laajan valikoiman: pienkorjauksista, sivous, tietokoneiden
-          huoltoon ja valvontakamerajärjestelmien asennukseen.
-        </h4>
-        <ServicesPageCards />
-        <Values/>
-      </section>
-    );
+  return (
+    <section className={s.servicesPage}>
+      <h2 className={s.title}>meidän palvelut</h2>
+      <h4 className={s.subtitle}>
+        Palvelumme kattavat laajan valikoiman: pienkorjauksista, sivous,
+        tietokoneiden huoltoon ja valvontakamerajärjestelmien asennukseen.
+      </h4>
+      <ServicesPageCards />
+      <Values />
+      <div className={s.ourWish}>
+        <h2 className={s.wishText}>
+          me hoidamme kotisi!{" "}
+          <span className={s.caption}>santerin palvelut.</span>
+        </h2>
+      </div>
+    </section>
+  );
 }
