6dfc70f63f10082b8455802aed8ec78afe242cda
Andrei <kaliarovich93@.gmail.com>
2024-05-03 23:52:26 +0300

    feat: add route to contacts

--- SanteriWebPage/src/routers/Routers.tsx
+++ SanteriWebPage/src/routers/Routers.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import ServicesPage from "../pages/ServicesPage/ServicesPage";
+import Contacts from "../pages/Contacts/Contacts";
 
 
 export default function Routers() {
@@ -9,12 +10,7 @@ export default function Routers() {
       <Route path="/" element={<Navigate to="/home" />} />
       <Route path="home" element={<Home />} />
       <Route path="services" element={<ServicesPage />} />
-      {/* <Route path="cart" element={<Cart />} />
-      <Route path="wishlist" element={<WishList />} />
-      <Route path="login" element={<Login />} />
-      <Route path="productDetails" element={<ProductDetails />} />
-      <Route path="shop" element={<Shop />} />
-      <Route path="signup" element={<Signup />} /> */}
+      <Route path="contacts" element={<Contacts />} />
     </Routes>
   );
 }
