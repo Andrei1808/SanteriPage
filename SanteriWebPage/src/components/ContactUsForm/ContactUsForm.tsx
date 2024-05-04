9cb976b0d6c3b22e304df3047baeab9f7d2bca05
Andrei <kaliarovich93@.gmail.com>
2024-05-04 23:39:12 +0300

    feat: add send info from form to email

--- SanteriWebPage/src/components/ContactUsForm/ContactUsForm.tsx
+++ SanteriWebPage/src/components/ContactUsForm/ContactUsForm.tsx
@@ -1,38 +1,55 @@
 import React from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
 import s from "./ContactUsForm.module.scss";
 import Button from "../Button/Button";
 
-type FormValues = {
-  firstName: string;
-  lastName: string;
-  email: string;
-  phoneNumber: string;
-  message: string;
-};
 
 export default function ContactUsForm() {
-  const { register, handleSubmit } = useForm<FormValues>();
-  const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data);
+
+  const onSubmit = async (event: { preventDefault: () => void; target: HTMLFormElement | undefined; }) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    console.log(formData);
+
+    formData.append("access_key", "3fe365a5-0b11-40d2-bd22-834d1e7c26e5");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: json,
+    }).then((res) => res.json());
+
+    if (res.success) {
+      console.log("Success", res);
+    }
+  };
 
   return (
     <section className={s.contactUsForm}>
-      <form className={s.formBody} onSubmit={handleSubmit(onSubmit)}>
+      <form className={s.formBody} onSubmit={()=>onSubmit}>
         <h2 className={s.title}>OTA YHTEYTTÄ</h2>
         <div className={s.formItem}>
           <input
             className={s.formInput}
             placeholder="nimi"
             type="text"
-            {...register("firstName")}
+            name="name"
+            minLength={2}
+            required
           />
         </div>
         <div className={s.formItem}>
           <input
             className={s.formInput}
             placeholder="sähköposti"
-            type="text"
-            {...register("email")}
+            type="email"
+            name="email"
+            required
           />
         </div>
         <div className={s.formItem}>
@@ -40,14 +57,18 @@ export default function ContactUsForm() {
             className={s.formInput}
             placeholder="puhelin"
             type="text"
-            {...register("phoneNumber")}
+            name="phone"
+            pattern="[0-9]*"
+            required
           />
         </div>
         <div className={s.formItem}>
           <textarea
             className={s.formTextarea}
             placeholder="viesti..."
-            {...register("message")}
+            name="message"
+            minLength={15}
+            required
           />
         </div>
         <div className={s.formItem}>
