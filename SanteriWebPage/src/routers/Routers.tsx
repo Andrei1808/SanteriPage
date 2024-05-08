5eefda45380a2e73d5bea5442ac52a1815ab6641
Andrei <kaliarovich93@.gmail.com>
2024-05-08 21:44:47 +0300

    feat: add royter to pc-service

--- SanteriWebPage/src/routers/Routers.tsx
+++ SanteriWebPage/src/routers/Routers.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import ServicesPage from "../pages/ServicesPage/ServicesPage";
 import Contacts from "../pages/Contacts/Contacts";
+import PcService from "../pages/PcService/PcService";
 
 
 export default function Routers() {
@@ -11,6 +12,7 @@ export default function Routers() {
       <Route path="home" element={<Home />} />
       <Route path="services" element={<ServicesPage />} />
       <Route path="contacts" element={<Contacts />} />
+      <Route path="computer-service" element={<PcService />} />
     </Routes>
   );
 }
