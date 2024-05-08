f1feefd5cdaee7c25332475d497f3be36f5f0fed
Andrei <kaliarovich93@.gmail.com>
2024-05-08 21:44:14 +0300

    refactro: refact code

--- SanteriWebPage/src/store/servicesData.ts
+++ SanteriWebPage/src/store/servicesData.ts
@@ -1,6 +1,6 @@
 export const services = [
   {
-    title: "Tietokone palvelu",
+    title: "Tietokonepalvelu",
     description:
       " Tarjoamme laadukkaita teknisiä palveluita, kuten uuden tietokoneen valmistelua, käyttöjärjestelmien asennusta ja vuosittaista huoltoa. Luota ammattitaitoiseen tiimiimme ja nauti luotettavasta ja korkeatasoisesta palvelusta!",
     icon: "src/assets/icons/pc1.png",
@@ -8,7 +8,7 @@ export const services = [
     id: "1",
   },
   {
-    title: "Remontti palvelu",
+    title: "Remonttipalvelu",
     description:
       " Kokoelma palveluitamme sisältää kaiken mitä tarvitset tilasi päivittämiseen: huonekalujen korjaamisesta ja verhoilusta aina asunnon remonttiin.",
     icon: "src/assets/icons/renovation.png",
@@ -16,7 +16,7 @@ export const services = [
     id: "2",
   },
   {
-    title: "Siivous palvelu",
+    title: "Siivouspalvelu",
     description:
       " Tiimimme tarjoaa laajan valikoiman siivouspalveluita erilaisiin tiloihin. Koosta ja haastavuudesta riippumatta olemme valmiita takaamaan tilasi puhtauden ja raikkauden. Luota meihin huolehtimaan kodistasi, toimistostasi tai mistä tahansa muusta paikasta, ja saat korkealaatuista palvelua ja moitteetonta tulosta.",
     icon: "src/assets/icons/cleaning.png",
@@ -24,7 +24,7 @@ export const services = [
     id: "3",
   },
   {
-    title: "Pihä palvelu",
+    title: "Pihäpalvelu",
     description:
       " Anna pihan aina kukoistaa puhtaudesta ja viihtyisyydestä huolehtivien ammattilaistemme avulla. Säästä ja säästä riippumatta olemme valmiita muuttamaan pihasi houkuttelevaksi ja hoidetuksi paikaksi. Luota meille pihan hoitoon, ja saat täydellisen järjestyksen ja moitteettoman ulkonäön.",
     icon: "src/assets/icons/outside.png",
@@ -32,7 +32,7 @@ export const services = [
     id: "4",
   },
   {
-    title: "Videovalvonta palvelu",
+    title: "Videovalvontapalvelu",
     description:
       "Luo luotettavuutta ja turvallisuutta tilaasi ammattitaitoisilla valvontajärjestelmien asennus- ja huoltopalveluillamme. Tarjoamme kattavia ratkaisuja kotisi, yrityksesi tai toimistosi suojaamiseksi, varmistaen korkean laadun ja luotettavuuden jokaisessa näkökulmassa.",
     icon: "src/assets/icons/cctv.png",
@@ -40,7 +40,7 @@ export const services = [
     id: "5",
   },
   {
-    title: "Valokuvaaja palvelu",
+    title: "Valokuvaajapalvelu",
     description:
       " Sukella kauneuden ja tunteiden maailmaan valokuvapalveluidemme avulla. Taltioimme elämäsi kirkkaimmat hetket luomalla upeita ja ainutlaatuisia kuvia. Olipa kyse sitten ammattimaisista valokuvista yrityksellesi tai ikimuistoisista muotokuvista perhealbumiin, olemme valmiita toteuttamaan ideoitasi lämmöllä ja ammattitaidolla.",
     icon: "src/assets/icons/photo.png",
