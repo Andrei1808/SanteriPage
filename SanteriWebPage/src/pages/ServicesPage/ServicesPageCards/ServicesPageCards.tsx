282cf76645cfddbb3c41f2e2ac3e33c245719838
Andrei <kaliarovich93@.gmail.com>
2024-05-20 22:12:28 +0300

    fat: add nav link

--- SanteriWebPage/src/pages/ServicesPage/ServicesPageCards/ServicesPageCards.tsx
+++ SanteriWebPage/src/pages/ServicesPage/ServicesPageCards/ServicesPageCards.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import s from "./ServicesPageCards.module.scss";
 import { services } from "../../../store/servicesData";
+import { NavLink } from "react-router-dom";
 
 export default function ServicesPageCards() {
   return (
     <div className={s.container}>
       {services.map((service, index) => (
-        <div className={s.card} key={index}>
-          <div className={s.imgWrapper}>
-            <img className={s.img} src={service.img} alt={service.title} />
+        <NavLink className={s.link} to={service.route}>
+          <div className={s.card} key={index}>
+            <div className={s.imgWrapper}>
+              <img className={s.img} src={service.img} alt={service.title} />
+            </div>
+            <div className={s.infoWrapper}>
+              <img className={s.icon} src={service.icon} alt={service.title} />
+              <p className={s.description}>{service.title}</p>
+            </div>
           </div>
-          <div className={s.infoWrapper}>
-            <img className={s.icon} src={service.icon} alt={service.title} />
-            <p className={s.description}>{service.title}</p>
-          </div>
-        </div>
+        </NavLink>
       ))}
     </div>
   );
