fe8e47c8b9cc32ff43988398e6c135a64b3277c5
Andrei <kaliarovich93@.gmail.com>
2024-05-29 23:45:28 +0300

    fix: remove motion

--- SanteriWebPage/src/components/Services/Services.tsx
+++ SanteriWebPage/src/components/Services/Services.tsx
@@ -1,25 +1,14 @@
 import React from "react";
 import ServicesCard from "./ServicesCard/ServicesCard";
 import s from "./Services.module.scss";
-import { motion } from "framer-motion";
-import { useInView } from "react-intersection-observer";
+
 
 export default function Services() {
-  const { ref, inView } = useInView({
-    triggerOnce: true,
-    threshold: 0.3,
-  });
   return (
     <section className={s.services}>
       <div className={s.title}>Mitä me teemme?</div>
-      <motion.div
-        ref={ref}
-        initial={{ opacity: 0, y: -50 }}
-        animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : -150 }}
-        transition={{ duration: 1.5 }}
-      >
-        <ServicesCard />
-      </motion.div>
+
+      <ServicesCard />
     </section>
   );
 }
