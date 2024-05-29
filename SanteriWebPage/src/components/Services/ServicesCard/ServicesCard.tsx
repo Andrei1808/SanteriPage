494d27b28e0c7989c4fea88e4003cbb530967c54
Andrei <kaliarovich93@.gmail.com>
2024-05-29 23:45:45 +0300

    feat: add motion animation

--- SanteriWebPage/src/components/Services/ServicesCard/ServicesCard.tsx
+++ SanteriWebPage/src/components/Services/ServicesCard/ServicesCard.tsx
@@ -1,17 +1,46 @@
-import React from 'react'
-import { services } from '../../../store/servicesData';
-import s from "./ServicesCard.module.scss"
+import React from "react";
+import { services } from "../../../store/servicesData";
+import s from "./ServicesCard.module.scss";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
-export default function () {
+export default function ServicesCard() {
+  const listVariants = {
+    visible: (i: number) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.5,
+        delay: i * 0.2,
+      },
+    }),
+    hidden: { opacity: 0, y: -30 },
+  };
+
   return (
     <div className={s.servicesCard}>
-      {services.map((service, index) => (
-        <div className={s.card} key={index}>
-              <img className={s.icon } src={service.icon} alt={service.title} />
-          <div  className={s.title }>{service.title}</div>
-          <div className={s.description }>{service.description}</div>
-        </div>
-      ))}
+      {services.map((service, i) => {
+        const { ref, inView } = useInView({
+          triggerOnce: true,
+          threshold: 0.1,
+        });
+
+        return (
+          <motion.div
+            className={s.card}
+            key={i}
+            variants={listVariants}
+            initial="hidden"
+            animate={inView ? "visible" : ""}
+            custom={i}
+            ref={ref}
+          >
+            <img className={s.icon} src={service.icon} alt={service.title} />
+            <div className={s.title}>{service.title}</div>
+            <div className={s.description}>{service.description}</div>
+          </motion.div>
+        );
+      })}
     </div>
   );
 }
