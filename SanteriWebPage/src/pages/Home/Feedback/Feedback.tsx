49ef95e6240f5435f875653046a468dec191e220
Andrei <kaliarovich93@.gmail.com>
2024-05-30 22:19:55 +0300

    fix: paddings

--- SanteriWebPage/src/pages/Home/Feedback/Feedback.tsx
+++ SanteriWebPage/src/pages/Home/Feedback/Feedback.tsx
@@ -15,38 +15,37 @@ export default function Feedback() {
 
       <motion.div
         ref={ref}
-        initial={{ opacity: 0, x: -50 }}
+        initial={{ opacity: 0 }}
         animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : -150 }}
         transition={{ duration: 1.5 }}
+        className={s.feedbackItems}
       >
-        <div className={s.feedbackItems}>
-          <div className={s.feedbackItem}>
-            <span className={s.icon}>
-              <VscFeedback />
-            </span>
-            <p className={s.feedbackText}>
-              "Yrityksenne tarjosi erinomaista palvelua, kun tarvitsin apua
-              kodin siivouksessa ja korjauksissa. Tiimi oli ammattitaitoinen ja
-              ystävällinen, ja he tekivät loistavaa työtä muuttaen tilani
-              mukavaksi ja viihtyisäksi. Olen erittäin tyytyväinen
-              lopputulokseen ja suosittelen teitä kaikille!"
-              <span className={s.feedbackAuthor}>Mikka</span>
-            </p>
-          </div>
-          <div className={s.feedbackItem}>
-            <span className={s.icon}>
-              <VscFeedback />
-            </span>
-            <p className={s.feedbackText}>
-              "Kiitos yrityksellenne moitteettomasta teknisestä tuesta ja
-              valokuvauspalveluista! Olen nauttinut ammattitaitoisesta ja
-              ystävällisestä asiakaspalvelusta jokaisessa vuorovaikutuksessa
-              kanssanne. Valokuvat ovat upeita ja ovat tulleet tärkeäksi osaksi
-              muistoistani. Suosittelen yritystänne kaikille, jotka etsivät
-              laadukasta ja luotettavaa palvelua."
-              <span className={s.feedbackAuthor}>Tuomas</span>
-            </p>
-          </div>
+        <div className={s.feedbackItem}>
+          <span className={s.icon}>
+            <VscFeedback />
+          </span>
+          <p className={s.feedbackText}>
+            "Yrityksenne tarjosi erinomaista palvelua, kun tarvitsin apua kodin
+            siivouksessa ja korjauksissa. Tiimi oli ammattitaitoinen ja
+            ystävällinen, ja he tekivät loistavaa työtä muuttaen tilani
+            mukavaksi ja viihtyisäksi. Olen erittäin tyytyväinen lopputulokseen
+            ja suosittelen teitä kaikille!"
+            <span className={s.feedbackAuthor}>Mikka</span>
+          </p>
+        </div>
+        <div className={s.feedbackItem}>
+          <span className={s.icon}>
+            <VscFeedback />
+          </span>
+          <p className={s.feedbackText}>
+            "Kiitos yrityksellenne moitteettomasta teknisestä tuesta ja
+            valokuvauspalveluista! Olen nauttinut ammattitaitoisesta ja
+            ystävällisestä asiakaspalvelusta jokaisessa vuorovaikutuksessa
+            kanssanne. Valokuvat ovat upeita ja ovat tulleet tärkeäksi osaksi
+            muistoistani. Suosittelen yritystänne kaikille, jotka etsivät
+            laadukasta ja luotettavaa palvelua."
+            <span className={s.feedbackAuthor}>Tuomas</span>
+          </p>
         </div>
       </motion.div>
     </section>
