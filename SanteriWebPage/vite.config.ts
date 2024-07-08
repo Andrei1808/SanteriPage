40e0237c66a5e4d910b551dd7a72cd76bc1a5ad8
Andrei <kaliarovich93@.gmail.com>
2024-07-08 21:01:45 +0300

    fix: vite config

--- SanteriWebPage/vite.config.ts
+++ SanteriWebPage/vite.config.ts
@@ -1,9 +1,27 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "src/assets/icons/*",
+          dest: "assets/icons",
+        },
+        {
+          src: "src/assets/images/*",
+          dest: "assets/images",
+        },
+        {
+          src: "src/assets/video/*",
+          dest: "assets/video",
+        },
+      ],
+    }),
+  ],
   build: {
     outDir: "dist",
+    assetsDir: "assets",
   },
 });
